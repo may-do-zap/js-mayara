eb74a5e3da7fed09b9f7882d3414d3dfda596df0
meu nome não carrega <00001085950074sp@al.educacao.sp.gov.br>
2024-06-28 13:35:17 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
 function setup() {
-    createCanvas(400, 400);
-  }
+  createCanvas(600, 600);
+  background("black");
+}
+
+function draw() {
+  stroke("blue");
+  fill("pink")
   
-  function draw() {
-    background(220);
+  if(mouseIsPressed){
+    rect(mouseX, mouseY, 20, 35)
   }
+
+}
